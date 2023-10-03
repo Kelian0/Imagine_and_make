a5aa32d5a722caf845c5ee005b31c68d9ae15f3e
Kelian0 <kelian.pons@gmail.com>
2023-10-03 11:33:29 +0200

    Calibration

--- Test_OpenCV.py
+++ Test_OpenCV.py
@@ -1,109 +1,39 @@
-import numpy as np 
+import numpy as np
 import cv2
- 
-# Check for left and right camera IDs
-# These values can change depending on the system
-CamL_id = "photo_imagine/5.jpg" # Camera ID for left camera
-CamR_id = "photo_imagine/6.jpg" # Camera ID for right camera
- 
-CamL= cv2.imread(CamL_id)
-CamR= cv2.imread(CamR_id)
- 
-# Reading the mapping values for stereo image rectification
-cv_file = cv2.FileStorage("data/stereo_rectify_maps.xml", cv2.FILE_STORAGE_READ)
-Left_Stereo_Map_x = cv_file.getNode("Left_Stereo_Map_x").mat()
-Left_Stereo_Map_y = cv_file.getNode("Left_Stereo_Map_y").mat()
-Right_Stereo_Map_x = cv_file.getNode("Right_Stereo_Map_x").mat()
-Right_Stereo_Map_y = cv_file.getNode("Right_Stereo_Map_y").mat()
-cv_file.release()
- 
-def nothing(x):
-    pass
- 
-cv2.namedWindow('disp',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('disp',600,600)
- 
-cv2.createTrackbar('numDisparities','disp',1,17,nothing)
-cv2.createTrackbar('blockSize','disp',5,50,nothing)
-cv2.createTrackbar('preFilterType','disp',1,1,nothing)
-cv2.createTrackbar('preFilterSize','disp',2,25,nothing)
-cv2.createTrackbar('preFilterCap','disp',5,62,nothing)
-cv2.createTrackbar('textureThreshold','disp',10,100,nothing)
-cv2.createTrackbar('uniquenessRatio','disp',15,100,nothing)
-cv2.createTrackbar('speckleRange','disp',0,100,nothing)
-cv2.createTrackbar('speckleWindowSize','disp',3,25,nothing)
-cv2.createTrackbar('disp12MaxDiff','disp',5,25,nothing)
-cv2.createTrackbar('minDisparity','disp',5,25,nothing)
- 
-# Creating an object of StereoBM algorithm
-stereo = cv2.StereoBM_create()
- 
-while True : 
- 
-    # Capturing and storing left and right camera images
-    imgL = CamL
-    imgR = CamR
 
-    imgR_gray = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY)
-    imgL_gray = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY)
+# Taille des cases de l'échiquier en mm
+square_size = 6.75
 
-    # Applying stereo image rectification on the left image
-    # Left_nice= cv2.remap(imgL_gray,
-    #             Left_Stereo_Map_x,
-    #             Left_Stereo_Map_y,
-    #             cv2.INTER_LANCZOS4,
-    #             cv2.BORDER_CONSTANT,
-    #             0)
-        
-    # Applying stereo image rectification on the right image
-    # Right_nice= cv2.remap(imgR_gray,
-    #             Right_Stereo_Map_x,
-    #             Right_Stereo_Map_y,
-    #             cv2.INTER_LANCZOS4,
-    #             cv2.BORDER_CONSTANT,
-    #             0)
+# Dimensions de l'échiquier (nombre de coins en largeur et en hauteur)
+chessboard_size = (8, 8)
 
-    # Updating the parameters based on the trackbar positions
-    numDisparities = cv2.getTrackbarPos('numDisparities','disp')*16
-    blockSize = cv2.getTrackbarPos('blockSize','disp')*2 + 5
-    preFilterType = cv2.getTrackbarPos('preFilterType','disp')
-    preFilterSize = cv2.getTrackbarPos('preFilterSize','disp')*2 + 5
-    preFilterCap = cv2.getTrackbarPos('preFilterCap','disp')
-    textureThreshold = cv2.getTrackbarPos('textureThreshold','disp')
-    uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio','disp')
-    speckleRange = cv2.getTrackbarPos('speckleRange','disp')
-    speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize','disp')*2
-    disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff','disp')
-    minDisparity = cv2.getTrackbarPos('minDisparity','disp')
-        
-    # Setting the updated parameters before computing disparity map
-    stereo.setNumDisparities(numDisparities)
-    stereo.setBlockSize(blockSize)
-    stereo.setPreFilterType(preFilterType)
-    stereo.setPreFilterSize(preFilterSize)
-    stereo.setPreFilterCap(preFilterCap)
-    stereo.setTextureThreshold(textureThreshold)
-    stereo.setUniquenessRatio(uniquenessRatio)
-    stereo.setSpeckleRange(speckleRange)
-    stereo.setSpeckleWindowSize(speckleWindowSize)
-    stereo.setDisp12MaxDiff(disp12MaxDiff)
-    stereo.setMinDisparity(minDisparity)
+# Liste pour stocker les coins détectés dans les images
+image_points = []
+object_points = []
 
-    # Calculating disparity using the StereoBM algorithm
-    disparity = stereo.compute(imgL_gray,imgR_gray)
-    # NOTE: Code returns a 16bit signed single channel image,
-    # CV_16S containing a disparity map scaled by 16. Hence it 
-    # is essential to convert it to CV_32F and scale it down 16 times.
+image_paths = [
+    "photo_calibration/1.jpg",
+]
 
-    # Converting to float32 
-    disparity = disparity.astype(np.float32)
+# Boucle sur les images de calibration
+for image_path in image_paths:
+    # Chargez l'image
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Scaling down the disparity values and normalizing them 
-    disparity = (disparity/16.0 - minDisparity)/numDisparities
+    # Tentez de détecter les coins de l'échiquier
+    ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
 
-    # Displaying the disparity map
-    cv2.imshow("disp",disparity)
+    if ret:
+        # Si les coins sont trouvés, ajoutez-les à la liste
+        image_points.append(corners)
 
-    # Close window using esc key
-    if cv2.waitKey(1) == 27:
-        break
+        # Générez les coordonnées 3D réelles des coins de l'échiquier
+        object_points.append(np.zeros((chessboard_size[0] * chessboard_size[1], 3), np.float32))
+        object_points[-1][:, :2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1, 2) * square_size
+
+# Maintenant, effectuez la calibration de la caméra
+ret, camera_matrix, distortion_coefficients, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, gray.shape[::-1], None, None)
+
+# Les résultats de calibration sont maintenant dans camera_matrix et distortion_coefficients
+np.savez('camera_calibration.npz', camera_matrix=camera_matrix, distortion_coefficients=distortion_coefficients)
